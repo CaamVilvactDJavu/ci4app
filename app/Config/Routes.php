ed9041b42dc93607f23de0c349bd60a2358158ae
CaamVilvactDJavu <caam.vilvact11@gmail.com>
2020-06-22 15:48:53 +0700

    Menambahkan Fitur Tambah Data Komik

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -33,9 +33,8 @@ $routes->setAutoRoute(true);
 // route since we don't have to scan directories.
 $routes->get('/', 'Pages::index');
 
-
+$routes->get('/komik/create', 'komik::create');
 $routes->get('/komik/(:segment)', 'Komik::detail/$1');
-
 
 $routes->get('/anime/(:segment)', 'Anime::detail/$1');
 
--- app/Views/komik/detail.php
+++ app/Views/komik/detail.php
@@ -15,11 +15,10 @@
                             <h5 class="card-title"><?= $komik['judul']; ?></h5>
                             <p class="card-text"><b>Penulis : </b><?= $komik['penulis']; ?> </p>
                             <p class="card-text"><small class="text-muted"><b>Penerbit : </b> <?= $komik['penerbit']; ?></small></p>
-
                             <a href="" class="btn btn-warning">Edit</a>
                             <a href="" class="btn btn-danger">Delete</a>
                             <br><br>
-                            <a href="/komik" class="btn btn-dark">Kembali ke daftar komik</a>
+                            <a href="/komik/" class="btn btn-dark">Kembali ke daftar komik</a>
                         </div>
                     </div>
                 </div>
